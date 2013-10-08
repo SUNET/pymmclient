458866317fe1b10b4cb64f54ecdc22e50802af70
Stefan Wold <swold@sunet.se>
2013-10-08 08:07:17 +0200

    Version 0.6.2

--- pymmclient/client.py
+++ pymmclient/client.py
@@ -8,7 +8,7 @@ from suds.client import Client
 from suds.cache import ObjectCache
 
 
-__version__ = '0.6'
+__version__ = '0.6.2'
 
 
 class MMClient(object):
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ testing_extras = [
 
 setup(
     name='pymmclient',
-    version='0.6.1',
+    version='0.6.2',
     description='Python API for the Swedish government service "Mina Meddelanden"',
     author='Stefan Wold',
     author_email='swold@sunet.se',
