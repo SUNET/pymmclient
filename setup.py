655e2afd2166d45481b55508ad79980a7eb2681b
Stefan Wold <swold@sunet.se>
2013-12-05 08:16:33 +0100

    Version 0.7
    - Service.wsdl implemented to send sealed deliveries
    - Updated dsig plugin to create sealed delivery signatures

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ testing_extras = [
 
 setup(
     name='pymmclient',
-    version='0.7-dev',
+    version='0.7',
     description='Python API for the Swedish government service "Mina Meddelanden"',
     author='Stefan Wold',
     author_email='swold@sunet.se',
