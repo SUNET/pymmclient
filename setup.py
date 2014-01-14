7087d7683c365cf752eaaaed5efab92732fbf3c1
Stefan Wold <swold@sunet.se>
2014-01-14 10:10:34 +0100

    Version 0.7.2

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ testing_extras = [
 
 setup(
     name='pymmclient',
-    version='0.7.1',
+    version='0.7.2',
     description='Python API for the Swedish government service "Mina Meddelanden"',
     author='Stefan Wold',
     author_email='swold@sunet.se',
